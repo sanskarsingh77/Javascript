42fd60efbb738d009519682c29863e760a9ee849
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-04-05 23:21:49 +0530

    Add files via upload

---
+++ day-14/main.js
@@ -0,0 +1,21 @@
+// *sync and async?
+
+console.log("I am a sync code") 
+setTimeout(()=>{
+    console.log("I am a async code")
+} , 5000)
+c
+
+
+// setInterval
+// setTimeout
+// fetch
+// axios
+// Promise
+// async / await 
+// XMLHttpRequest
+
+
+
+// *asynx js?
+// *js is not async by default
