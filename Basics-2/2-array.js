5a644b1c079a8ce2215974416233dca566517124
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-05-04 23:51:20 +0530

    Add files via upload

---
+++ Basics-2/2-array.js
@@ -0,0 +1,35 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// spread operator works similar to concat but used more often than concat because works for more than 2 array concatination
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// array ke andar array ho aur tmko saaare elemnt ko ek hi array mekrna ho or spred krns ho to flst use jrte  hai infinity to depth btata rhi hai 
+// infinity isliye liya ke kitne bhi arrray ke andar array ho saaare ek array me ajaye  
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+// of se bhi hum scores ki value ko ek array me store krate hai
