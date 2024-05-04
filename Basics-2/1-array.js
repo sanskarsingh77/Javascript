5080e4760ab995fc071cc50167d8c515e62609f4
sanskarsingh77 <132278061+sanskarsingh77@users.noreply.github.com>
2024-05-04 23:51:08 +0530

    Add files via upload

---
+++ Basics-2/1-array.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// unshift starting me insert karanee ke liye use hota hai
+// myArr.shift()
+// shift starting se elment remove krne ke liye use hota hai
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+// slice me original array me se sliced array nikal jata hai
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+// splice me original array me se spliced array nikal jata hai aur alag arry bn leta hai
